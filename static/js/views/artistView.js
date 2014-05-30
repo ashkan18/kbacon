8a9579b8b7097a1d5129edaadea8ae0ba34a4131
Ashkan <anasseri@apperian.com>
2014-05-29 21:36:06 -0400

    Better js after running grunt and jshint checks

--- static/js/views/artistView.js
+++ static/js/views/artistView.js
@@ -7,12 +7,12 @@ window.ArtistView = Backbone.View.extend({
     render: function () {
         // response coming back from the server has a path which shows the path from
         // requested artist to Kevin Bacon
-        var path = this.model.toJSON()['path'];
+        var path = this.model.toJSON().path;
         if (path.length === 0) {
             // there was no path, it's either we are searching for Kevin Bacon himself
             // or there was no path from this artist to Kevin Bacon (infinite distance)
             var message = '';
-            if (this.model.toJSON()['artist_name'] === 'Kevin Bacon'){
+            if (this.model.toJSON().artist_name === 'Kevin Bacon'){
                 message = 'Are you kidding me?! Kevin Bacon knows himself!';
             } else {
                 message = 'Hmmmm..... Looks like this artist is pretty far from KB!';
@@ -24,7 +24,7 @@ window.ArtistView = Backbone.View.extend({
             // because of the fact that first item doesn't have a movie we have to decrease the path by 1
             // and then divide it by two to get the actual distance
             var distance = (path.length - 1) / 2;
-            $(this.el).append(this.template({'distance': distance, 'path':path, 'name': this.model.toJSON()['artist_name']}));
+            $(this.el).append(this.template({'distance': distance, 'path':path, 'name': this.model.toJSON().artist_name}));
         }
         return this;
     }
